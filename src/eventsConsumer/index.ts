e6f115c785a01e0437a90551714657909be90c78
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-04 17:56:33 +0530

    add: eventsConsumer, postgres, storage interface

---
+++ src/eventsConsumer/index.ts
@@ -0,0 +1,51 @@
+import { initPulsar } from '../lib/pulsar.js'
+import config from '../lib/config.js'
+import { Postgres } from './postgres.js'
+import type { Event } from '../lib/schema.js'
+
+// Database
+const db = new Postgres()
+try {
+	await db.init({
+		host: config.dbHost,
+		port: config.dbPort,
+		user: config.dbUser,
+		password: config.dbPassword,
+		database: config.dbName,
+	})
+} catch (error) {
+	console.error('Failed to initialize database:', error)
+	process.exit(1)
+}
+
+async function consume(event: Event) {
+	await db.save(event)
+}
+
+// Pulsar Consumer
+
+let client = initPulsar()
+
+const subscriptionName =
+	'eventsConsumer-' + Math.random().toString(36).substring(2, 15)
+
+let consumer = await client.subscribe({
+	topic: config.pulsarTopic,
+	subscription: subscriptionName,
+	subscriptionType: 'Shared',
+	// TODO: Dead letter policy
+	listener: async (message, consumer) => {
+		const event: Event = JSON.parse(message.getData().toString()) // TODO: Handle parse errors, preferably with zod
+
+		console.log(`received: ${event.eventID}`)
+
+		try {
+			await consume(event)
+			console.log('Event consumed successfully')
+			consumer.acknowledge(message)
+		} catch (error) {
+			console.error('Error consuming message:', error)
+			consumer.negativeAcknowledge(message)
+		}
+	},
+})
---
+++ src/eventsConsumer/postgres.ts
@@ -0,0 +1,52 @@
+import type { Event } from '../lib/schema.js'
+import type { IStorage } from '../lib/storage.js'
+import { Client } from 'pg'
+
+export class Postgres implements IStorage {
+	client: Client = null as unknown as Client
+
+	async init(config: any): Promise<void> {
+		this.client = new Client({
+			host: config.host,
+			port: config.port,
+			user: config.user,
+			password: config.password,
+			database: config.database,
+		})
+
+		await this.client.connect()
+
+		const res = await this.client.query(`
+			CREATE TABLE IF NOT EXISTS events (
+				id UUID PRIMARY KEY,
+				site_id UUID NOT NULL,
+				timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+				user_agent TEXT,
+				location TEXT
+			)
+		`)
+	}
+
+	async save(event: Event): Promise<void> {
+		const res = await this.client.query(
+			'INSERT INTO events(id, site_id, timestamp, user_agent, location) VALUES($1, $2, $3, $4, $5) RETURNING *',
+			[
+				event.eventID,
+				event.siteID,
+				new Date(event.timestamp),
+				event.userAgent,
+				event.location,
+			]
+		)
+		if (res.rowCount !== 1) {
+			throw new Error('Failed to save event')
+		}
+	}
+
+	async get(eventId: string): Promise<any> {
+		// Retrieve event from Postgres database here
+	}
+	async close(): Promise<void> {
+		await this.client.end()
+	}
+}
