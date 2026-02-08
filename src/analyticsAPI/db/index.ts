6c74e985f8ba574a7186d86f567309de73ff2533
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-09 00:21:56 +0530

    fix: implement database type check in createDB function

--- src/analyticsAPI/db/index.ts
+++ src/analyticsAPI/db/index.ts
@@ -2,8 +2,13 @@ import type { IDatabase } from './interface.js'
 import { Postgres } from './postgres.js'
 
 export async function createDB(config: any): Promise<IDatabase> {
-	// TODO: add logic to check whether postgres or cassandra or something
-	const db = new Postgres()
+	let db: IDatabase
+	if (config.dbType === 'postgres') {
+		db = new Postgres()
+	} else {
+		throw new Error(`Unsupported database type: ${config.dbType}`)
+	}
+
 	await db.init(config)
 	return db
 }
