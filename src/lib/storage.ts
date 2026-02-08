e8d5ecac680369e0fdcdbce507736d5aa6a85ef6
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-09 01:26:35 +0530

    chore: update storage interface

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,7 +1,6 @@
 import type { Event } from './schema.js'
 
 export interface IStorage {
-	init(config: any): Promise<void>
 	save(event: Event): Promise<void>
 	get(eventId: any): any
 	close(): void
