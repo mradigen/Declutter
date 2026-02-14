1d7b0c99d56f6991b0fdb134aea275176bd3c82a
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-14 20:19:07 +0530

    fix: coerce types in zod

--- src/analyticsAPI/types.ts
+++ src/analyticsAPI/types.ts
@@ -1,26 +1,26 @@
 import z from 'zod'
 
 export const EventsByTimeParamsSchema = z.object({
-	siteID: z.uuid(),
-	startTime: z.number().int().nonnegative(),
-	endTime: z.number().int().nonnegative(),
+	// siteID: z.uuid(),
+	startTime: z.coerce.number().int().nonnegative(),
+	endTime: z.coerce.number().int().nonnegative(),
 	interval: z.string().min(1).optional(),
 })
 
 export type EventsByTimeParams = z.infer<typeof EventsByTimeParamsSchema>
 
 export const UserAgentCountParamsSchema = z.object({
-	siteID: z.uuid(),
-	startTime: z.number().int().nonnegative(),
-	endTime: z.number().int().nonnegative(),
+	// siteID: z.uuid(),
+	startTime: z.coerce.number().int().nonnegative(),
+	endTime: z.coerce.number().int().nonnegative(),
 })
 
 export type UserAgentCountParams = z.infer<typeof UserAgentCountParamsSchema>
 
 export const LocationCountParamsSchema = z.object({
-	siteID: z.uuid(),
-	startTime: z.number().int().nonnegative(),
-	endTime: z.number().int().nonnegative(),
+	// siteID: z.uuid(),
+	startTime: z.coerce.number().int().nonnegative(),
+	endTime: z.coerce.number().int().nonnegative(),
 })
 
 export type LocationCountParams = z.infer<typeof LocationCountParamsSchema>
