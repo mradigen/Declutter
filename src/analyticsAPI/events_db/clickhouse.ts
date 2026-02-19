955c4f14f5f66cb4df608a0c74cc1def9f0645a5
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-19 19:31:29 +0530

    feat: clickhouse implementation

--- src/analyticsAPI/events_db/clickhouse.ts
+++ src/analyticsAPI/events_db/clickhouse.ts
@@ -1,4 +1,3 @@
-import { Pool } from 'pg'
 import { createClient } from '@clickhouse/client'
 import type { Site, User } from '../../lib/schema.js'
 import type {
@@ -9,82 +8,69 @@ import type {
 import type { IEventsDB } from './IEventsDB.js'
 import type { NodeClickHouseClient } from '@clickhouse/client/dist/client.js'
 
-// TODO: Use Clickhouse client etc.
 export class Clickhouse implements IEventsDB {
-	// client: Pool = null as unknown as Pool
 	client: NodeClickHouseClient
 
 	constructor(config: any) {
 		this.client = createClient({
-			host: `${config.host}:${config.port}`,
-			// port: config.port,
-			// username: config.user,
+			url: `http://${config.host}:${config.port}`,
+			username: config.user,
 			password: config.password,
-			// database: config.database,
+			database: config.database,
 		})
 	}
-
-	// 	this.client = new Pool({
-	// 		host: config.host,
-	// 		port: config.port,
-	// 		user: config.user,
-	// 		password: config.password,
-	// 		database: config.database,
-
-	// 		max: 20,
-	// 		idleTimeoutMillis: 30000,
-	// 		connectionTimeoutMillis: 2000,
-	// 		maxLifetimeSeconds: 60,
-	// 	})
-	// }
 
 	async eventsByTime(site: Site, params: EventsByTimeParams) {
 		const { startTime, endTime, interval } = params
 
-		const res = await this.client.query(
-			`
-			SELECT time_bucket($4, timestamp) AS time, COUNT(*)
-			FROM events
-			WHERE site_id = $1 AND timestamp >= to_timestamp($2) AND timestamp <= to_timestamp($3)
-			GROUP BY time`,
-			[site.site_id, startTime, endTime, interval]
-		)
-		return res.rows
+		const res = await this.client.query({
+			// TODO: Make these materialized views for better performance
+			query: `
+				SELECT toStartOfInterval(timestamp, INTERVAL ${interval}) AS time, COUNT(*) as count
+				FROM events
+				WHERE site_id='${site.site_id}' AND timestamp >= toDateTime(${startTime}) AND timestamp <= toDateTime(${endTime})
+				GROUP BY time
+				ORDER BY time
+			`,
+			format: 'JSONEachRow',
+		})
+
+		return await res.json()
 	}
 
 	async userAgentCount(site: Site, params: UserAgentCountParams) {
 		const { startTime, endTime } = params
 
-		const res = await this.client.query(
-			`
-			SELECT user_agent, COUNT(*)
-			FROM events
-			WHERE site_id = $1 AND timestamp >= to_timestamp($2) AND timestamp <= to_timestamp($3)
-			GROUP BY user_agent`,
-			[site.site_id, startTime, endTime]
-		)
-		return res.rows
+		const res = await this.client.query({
+			query: `
+				SELECT user_agent, COUNT(*) as count
+				FROM events
+				WHERE site_id='${site.site_id}' AND timestamp >= toDateTime(${startTime}) AND timestamp <= toDateTime(${endTime})
+				GROUP BY user_agent
+			`,
+			format: 'JSONEachRow',
+		})
+
+		return await res.json()
 	}
 
 	async locationCount(site: Site, params: LocationCountParams) {
 		const { startTime, endTime } = params
 
-		const res = await this.client.query(
-			`
-			SELECT location, COUNT(*)
-			FROM events
-			WHERE site_id = $1 AND timestamp >= to_timestamp($2) AND timestamp <= to_timestamp($3)
-			GROUP BY location`,
-			[site.site_id, startTime, endTime]
-		)
-		return res.rows
+		const res = await this.client.query({
+			query: `
+				SELECT location, COUNT(*) as count
+				FROM events
+				WHERE site_id='${site.site_id}' AND timestamp >= toDateTime(${startTime}) AND timestamp <= toDateTime(${endTime})
+				GROUP BY location
+			`,
+			format: 'JSONEachRow',
+		})
+
+		return await res.json()
 	}
 
-	// async query(text: string, params?: any[]): Promise<any> {
-	// 	return this.client.query(text, params)
-	// }
-
 	async close(): Promise<void> {
-		await this.client.end()
+		await this.client.close()
 	}
 }
