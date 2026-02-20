b4a65c886a310faa5c0bf3cf1e381032501a7f38
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-20 23:24:08 +0530

    fix: upgrade event validation

--- src/eventsProducer/index.ts
+++ src/eventsProducer/index.ts
@@ -1,6 +1,6 @@
 import { serve } from '@hono/node-server'
-import { sValidator } from '@hono/standard-validator'
 import { Hono } from 'hono'
+import { validator } from 'hono/validator'
 import config from '../lib/config.js'
 import { initPulsar } from '../lib/pulsar.js'
 import { EventSchema, type Event } from '../lib/schema.js'
@@ -21,32 +21,46 @@ let producer = await client.createProducer({
 })
 console.log('Pulsar Producer initialized')
 
-app.post('/event', sValidator('json', EventSchema), (c) => {
-	const event: Event = c.req.valid('json')
+app.post(
+	'/event',
+	validator('json', (value) => value),
+	async (c) => {
+		const event: Event = c.req.valid('json') as Event
 
-	if (!checkSiteID(event.site_id)) {
-		// c.status(400)
-		// return c.text('Invalid siteID')
-		c.status(202) // sends 202 to prevent leaking information about valid siteIDs
+		if (!checkSiteID(event.site_id)) {
+			if (config.mode === 'development') {
+				c.status(400)
+				return c.text('Invalid siteID')
+			}
+
+			c.status(202) // sends 202 to prevent leaking information about valid siteIDs
+			return c.text('Event produced')
+		}
+
+		event.event_id = crypto.randomUUID()
+		event.timestamp = Date.now()
+
+		let validatedEvent: Event
+		try {
+			validatedEvent = await EventSchema.parseAsync(event)
+		} catch (error) {
+			c.status(400)
+			return c.text('Invalid event data: ' + error)
+		}
+
+		producer.send({
+			data: Buffer.from(JSON.stringify(validatedEvent)),
+		})
+
+		c.status(202)
 		return c.text('Event produced')
 	}
-
-	event.timestamp = Date.now()
-
-	// XXX: Add more validations
-
-	producer.send({
-		data: Buffer.from(JSON.stringify(event)),
-	})
-
-	c.status(202)
-	return c.text('Event produced')
-})
+)
 
 serve(
 	{
 		fetch: app.fetch,
-		port: 3000,
+		port: config.producer.listenPort,
 	},
 	(info) => {
 		console.log(`Server is running on http://localhost:${info.port}`)
