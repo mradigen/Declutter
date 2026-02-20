4497e78bd87f15d21c7adb29340b01356efa0d5d
mradigen <55953083+mradigen@users.noreply.github.com>
2026-02-20 22:22:28 +0530

    chore: unify types into a single AnalyticsParams type

--- src/analyticsAPI/events_db/IEventsDB.ts
+++ src/analyticsAPI/events_db/IEventsDB.ts
@@ -1,13 +1,10 @@
 import type { Site } from '../../lib/schema.js'
-import type {
-	EventsByTimeParams,
-	LocationCountParams,
-	UserAgentCountParams,
-} from '../types.js'
+import type { AnalyticsParams } from '../types.js'
 
 export interface IEventsDB {
-	eventsByTime(site: Site, params: EventsByTimeParams): Promise<any>
-	userAgentCount(site: Site, params: UserAgentCountParams): Promise<any>
-	locationCount(site: Site, params: LocationCountParams): Promise<any>
+	eventsByTime(site: Site, params: AnalyticsParams): Promise<any>
+	userAgentCount(site: Site, params: AnalyticsParams): Promise<any>
+	locationCount(site: Site, params: AnalyticsParams): Promise<any>
+	referrerCount(site: Site, params: AnalyticsParams): Promise<any>
 	close(): Promise<void>
 }
--- src/analyticsAPI/types.ts
+++ src/analyticsAPI/types.ts
@@ -1,26 +1,10 @@
 import z from 'zod'
 
-export const EventsByTimeParamsSchema = z.object({
+export const AnalyticsParamsSchema = z.object({
 	// siteID: z.uuid(),
 	startTime: z.coerce.number().int().nonnegative(),
 	endTime: z.coerce.number().int().nonnegative(),
 	interval: z.string().min(1).optional(),
 })
 
-export type EventsByTimeParams = z.infer<typeof EventsByTimeParamsSchema>
-
-export const UserAgentCountParamsSchema = z.object({
-	// siteID: z.uuid(),
-	startTime: z.coerce.number().int().nonnegative(),
-	endTime: z.coerce.number().int().nonnegative(),
-})
-
-export type UserAgentCountParams = z.infer<typeof UserAgentCountParamsSchema>
-
-export const LocationCountParamsSchema = z.object({
-	// siteID: z.uuid(),
-	startTime: z.coerce.number().int().nonnegative(),
-	endTime: z.coerce.number().int().nonnegative(),
-})
-
-export type LocationCountParams = z.infer<typeof LocationCountParamsSchema>
+export type AnalyticsParams = z.infer<typeof AnalyticsParamsSchema>
